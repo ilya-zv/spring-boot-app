65e9092d9f9341ce2bf3b0f1ca0a4e43b9fd3612
ilya-zv <zeshot@yandex.ru>
2021-11-14 22:15:57 +0300

    Add banner.txt; Update interface & impl LibraryDao; Update ShellServiceImpl(new methods);

--- src/main/java/ru/zvor/spring/dao/LibraryDao.java
+++ src/main/java/ru/zvor/spring/dao/LibraryDao.java
@@ -7,10 +7,14 @@ import java.util.List;
 
 public interface LibraryDao {
     public void insertAuthor(Author author);
-    public void insertBookAndAuthor (Book book, Author author);
-    public Book getBookById (long id);
-    public Author getAuthorById (long author_id);
-    public List<Book> getAllBookByAuthor (long author_id);
-    public List<Author> getAllAuthorByBookName (String book_name);
-    public List<Author> getAllAuthorByBookId (long id);
+    public void insertBookAndAuthor(Book book, Author author);
+    public Book getBookById(long id);
+    public List<Book> getAllBook();
+    public List<Author> getAllAuthor();
+    public Author getAuthorById(long author_id);
+    public List<Book> getAllBookByAuthor(long author_id);
+    public List<Author> getAllAuthorByBookName(String book_name);
+    public List<Author> getAllAuthorByBookId(long id);
+    public void deleteByBookId (long book_id);
+    public void deleteByAuthorId (long author_id);
 }
--- src/main/java/ru/zvor/spring/service/ShellServiceImpl.java
+++ src/main/java/ru/zvor/spring/service/ShellServiceImpl.java
@@ -1,11 +1,16 @@
 package ru.zvor.spring.service;
 
+import com.sun.el.stream.Stream;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import ru.zvor.spring.dao.LibraryDao;
+import ru.zvor.spring.domain.Author;
+import ru.zvor.spring.domain.Book;
+
+import java.util.Arrays;
 
 @ShellComponent
-public class ShellServiceImpl implements ShellService {
+public class ShellServiceImpl {
 
     private LibraryDao dao;
     private IOService ioService;
@@ -15,9 +20,44 @@ public class ShellServiceImpl implements ShellService {
         this.ioService = ioService;
     }
 
+    @ShellMethod(value = "Добавляет автора в базу данных", key = "ia")
+    public void insertAuthor(int id, String name) {
+        dao.insertAuthor(new Author(id, name));
+        System.out.println("Автор успешно добавлен в БД.");
+    }
+
+    @ShellMethod(value = "Добавляет автора и книгу в базу данных", key = "iab")
+    public void insertBookAndAuthor(int book_id, String book_name, int author_id, String author_name) {
+        dao.insertBookAndAuthor(new Book(book_id, book_name, author_id), new Author(author_id, author_name));
+        System.out.println("Автор и книга успешно добавлены в БД.");
+    }
+
     @ShellMethod(value = "Выводит книгу по ID", key = "gbbi")
     public void getBookById(long id) {
         ioService.whrite(dao.getBookById(id).toString());
     }
+
+    @ShellMethod(value = "Выводит все книги в БД", key = "gab")
+    public void getAllBook() {
+        dao.getAllBook().stream().map((i) -> "Книга: " + i.getId() + ". Название: " + i.getName() + ". Автор № " + i.getAuthor()).forEach(System.out::println);
+    }
+
+    @ShellMethod(value = "Выводит всех авторов в БД", key = "gaa")
+    public void getAllAuthor() {
+        dao.getAllAuthor().stream().map((i) -> "Автор: " + i.getId() + ". Ф.И.О: " + i.getName()).forEach(System.out::println);
+    }
+
+    @ShellMethod(value = "Удаляет книгу из БД", key = "db")
+    public void deleteBook(int id) {
+        dao.deleteByBookId(id);
+        ioService.whrite("Книга удалена!");
+    }
+
+    @ShellMethod(value = "Удаляет автора из БД", key = "da")
+    public void deleteAuthor(long id) {
+        dao.deleteByAuthorId(id);
+    }
+
+
 
 }
